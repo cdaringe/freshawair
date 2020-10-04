cb2b0908bf9ab6bf97ed21020cd2b22976a166a9
cdaringe <christopher.dieringer@gmail.com>
2020-10-03 22:22:08 -0700

    wip

--- bin/Fresh.ml
+++ bin/Fresh.ml
@@ -24,37 +24,43 @@ let on_sense_request conn () = read_sensors () >>= on_sensors_read conn
 
 let on_sense_fail exn =
   Server.respond_string ~headers:json_headers ~status:`Bad_gateway
-    ~body:(Printf.sprintf "Failed to read sensors: %s" (Exn.to_string exn)) ()
+    ~body:(Printf.sprintf "Failed to read sensors: %s" (Exn.to_string exn))
+    ()
 
-let stream_air_stats_from_pg_to_http (conn: Postgresql.connection) () =
-let c = conn in
-ignore (c#exec ~expect:[Command_ok] "begin");
-ignore (
-  c#exec
-    ~expect:[Command_ok]
-    "declare air_cursor cursor for select * from sensor_stats");
-let rec loop () =
-  let res = c#exec ~expect:[Tuples_ok] "FETCH IN air_cursor" in
-  if res#ntuples <> 0 then (
-    let tpl = res#get_tuple 0 in
-    Console.log tpl;
-    (* print_string tpl.(0); *)
-    (* for i = 1 to Array.length tpl - 1 do print_string (" " ^ tpl.(i)) done; *)
-    (* Console.log (); *)
-    loop ()) in
-loop ();
-ignore (c#exec ~expect:[Command_ok] "CLOSE air_cursor");
-ignore (c#exec ~expect:[Command_ok] "END")
+let stream_air_stats_from_pg_to_http (c : Postgresql.connection) =
+  let stat_stream, push = Lwt_stream.create () in
+  let rec get_next_stat () =
+    let cursor_res = c#exec ~expect:[ Tuples_ok ] "FETCH IN air_cursor" in
+    if cursor_res#ntuples <> 0 then (
+      push
+      @@ Some (String.concat ~sep:"," @@ Array.to_list @@ cursor_res#get_tuple 0);
+      get_next_stat () )
+    else (
+      push @@ Some "]";
+      push None )
+  in
+  let finally () =
+    ignore (c#exec ~expect:[ Command_ok ] "CLOSE air_cursor");
+    ignore (c#exec ~expect:[ Command_ok ] "END")
+  in
+  let get_body () =
+    ignore (Lwt_stream.closed stat_stream >>= fun _ -> Lwt.return @@ finally ());
+    push @@ Some "[";
+    get_next_stat ();
+    `Stream stat_stream
+  in
+  ignore (c#exec ~expect:[ Command_ok ] "BEGIN");
+  ignore
+    (c#exec ~expect:[ Command_ok ]
+       "DECLARE air_cursor CURSOR FOR SELECT * FROM sensor_stats");
+  Server.respond ~headers:json_headers ~status:`OK ~body:(get_body ()) ()
 
-let create_server_handler ~conn _conn_id (req: Request.t) _body =
+let create_server_handler ~conn _conn_id (req : Request.t) _body =
   match req.resource with
-    | "/air/stats" ->
-      let () = stream_air_stats_from_pg_to_http conn () in
-      Server.respond_string ~headers:json_headers ~status:`OK ~body:"sup!" ()
-    | _ ->  Lwt.catch (on_sense_request conn) on_sense_fail
+  | "/air/stats" -> stream_air_stats_from_pg_to_http conn
+  | _ -> Lwt.catch (on_sense_request conn) on_sense_fail
 
 exception InitError of string
-
 
 let with_connection () : Ezpostgresql.connection Lwt.t =
   Lib.Db.create_connection ~host:"localhost" ~password:"fresh" ~user:"fresh" ()
@@ -68,7 +74,9 @@ let server ~port =
   let onconn = create_server_handler ~conn in
   Console.log @@ "Server " ^ Pastel.green "started" ^ " on port "
   ^ Pastel.greenBright @@ string_of_int port;
-  Server.create ~mode:(`TCP (`Port port)) (Server.make ~callback:onconn ()) ~on_exn:(fun err ->
-    Console.error err)
+  Server.create
+    ~mode:(`TCP (`Port port))
+    (Server.make ~callback:onconn ())
+    ~on_exn:(fun err -> Console.error err)
 
 let () = ignore (Lwt_main.run @@ server ~port:8000)
